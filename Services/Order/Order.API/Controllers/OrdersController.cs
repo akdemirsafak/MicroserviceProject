07c139f282a94902f268c7b467f80e2e67a4565e
akdemirsafak <akdemirsafak@gmail.com>
2023-05-26 14:57:46 +0300

    Controller Ok.MediatR Package Changed

---
+++ Services/Order/Order.API/Controllers/OrdersController.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Order.Application.Commands.CreateOrder;
+using Order.Application.Queries.GetOrdersByUserId;
+using SharedLibrary.ControllerBases;
+using SharedLibrary.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Order.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : CustomBaseController
+    {
+        private readonly IMediator _mediator;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public OrdersController(IMediator mediator, ISharedIdentityService sharedIdentityService)
+        {
+            _mediator = mediator;
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+        // GET: api/<OrdersController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var response = await _mediator.Send(new GetOrdersByUserIdQuery { UserId=_sharedIdentityService.GetUserId });
+            return CreateActionResult(response);
+        }
+
+        // GET api/<OrdersController>/5
+        [HttpPost]
+        public async Task<IActionResult> SaveOrder(CreateOrderCommand request)
+        {
+            var response = await _mediator.Send(request);
+            return CreateActionResult(response);
+        }
+
+    }
+}
--- Services/Order/Order.API/Program.cs
+++ Services/Order/Order.API/Program.cs
@@ -1,21 +1,30 @@
 using System.Reflection;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Order.Infrastructure;
+using SharedLibrary.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+builder.Services.AddDbContext<OrderDbContext>(opt => {
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+    opt => {
+        opt.MigrationsAssembly(Assembly.GetAssembly(typeof(OrderDbContext))!.GetName().Name); //DbContext'in bulunduğu class'ı dahil edelim.DbContext'den türetilen OrderDbContext'i dahil eder.
+    });
+});
+
+builder.Services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+builder.Services.AddHttpContextAccessor();
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<OrderDbContext>(opt=>{
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
-    opt=>{
-        opt.MigrationsAssembly(Assembly.GetAssembly(typeof(OrderDbContext))!.GetName().Name); //DbContext'in bulunduğu class'ı dahil edelim.DbContext'den türetilen OrderDbContext'i dahil eder.
-    });
-});
 
 var app = builder.Build();
 
