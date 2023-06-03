5de3936ffe257ee3c42d98bffa2894dc37a648af
akdemirsafak <akdemirsafak@gmail.com>
2023-06-04 00:53:16 +0300

    IdentityModel and Service added.Client and ServiceApi Settings added with Option Pattern

---
+++ Frontend/Web/Models/SignInInput.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models;
+
+public class SignInInput
+{
+    [DisplayName("Mail adresi : ")]
+    [DataType(DataType.EmailAddress)]
+    public required string Email { get; set; }
+    [DisplayName("Parola : ")]
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+    [DisplayName("Beni hatırla : ")]
+    public bool RememberMe { get; set; }
+}
---
+++ Frontend/Web/Services/ServiceApiSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Web.Services;
+
+public class ServiceApiSettings
+{
+    public string BaseUri { get; set; } //IdentityServer 
+    public string PhotoStockUri { get; set; }
+}
