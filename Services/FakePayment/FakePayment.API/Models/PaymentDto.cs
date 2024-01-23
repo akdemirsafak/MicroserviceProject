826c4b2d41098377f3e22a8636b9fad49ad4947b
akdemirsafak <akdemirsafak@gmail.com>
2024-01-23 23:55:46 +0300

    Add OrderDto and update PaymentDto with Order information

--- Services/FakePayment/FakePayment.API/Models/PaymentDto.cs
+++ Services/FakePayment/FakePayment.API/Models/PaymentDto.cs
@@ -7,4 +7,7 @@ public class PaymentDto
     public string ExpirationDate { get; set; }
     public string CVV { get; set; }
     public decimal TotalPrice { get; set; }
+    public OrderDto Order { get; set; }
 }
+//Ödeme işleme alındıktan sonra sipariş oluşturulması için RabbitMQ'ya sipariş mesajı gönderilmesi gerekiyor.PaymentDto'nun içerisinde sipariş bilgileri de bulunması gerekli.
+//Update'imizi bu alanlar için yapıyoruz. 
