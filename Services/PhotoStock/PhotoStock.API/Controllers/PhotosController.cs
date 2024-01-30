4e4dc55ba0c5ae449b9ddffa016a896126fa0a11
akdemirsafak <akdemirsafak@gmail.com>
2024-01-30 22:08:23 +0300

    Add Dockerfiles and update docker-compose.yml PhotoStockController HttpDelete Attribute added.

--- Services/PhotoStock/PhotoStock.API/Controllers/PhotosController.cs
+++ Services/PhotoStock/PhotoStock.API/Controllers/PhotosController.cs
@@ -16,19 +16,20 @@ namespace PhotoStock.API.Controllers
             //Async başlayan bir işlem sadece hata fırlatarak sonlandırılabilir.CancellationToken'da bu mantıkla çalışır.
             if (photo is not null && photo.Length > 0)
             {
-                var path=Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/photos",photo.FileName);
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
                 using var stream = new FileStream(path, FileMode.Create);
                 await photo.CopyToAsync(stream, cancellationToken);
                 //var returnPath="photos/"+photo.FileName; //bu endpoint'in response u görselin adresi olacak.
-                PhotoDto returnPathDto=new() { Url=photo.FileName};
+                PhotoDto returnPathDto = new() { Url = photo.FileName };
                 return CreateActionResult(Response<PhotoDto>.Success(returnPathDto, 201));
             }
             return CreateActionResult(Response<PhotoDto>.Fail("Photo Empty", 400));
         }
 
+        [HttpDelete]
         public IActionResult PhotoDelete(string photoUrl)
         {
-            var path=Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/photos",photoUrl);
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
 
             if (!System.IO.File.Exists(path))
             {
